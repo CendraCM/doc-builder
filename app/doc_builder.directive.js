e507caf954fd06a535e58b7b5e7a1a07c261a101
Agustin Moyano <agustin.moyano@gmail.com>
2016-10-28 15:25:17 -0300

    mejorando cuando se decide que los elementos de un array van a ser todos iguales o no

--- app/doc_builder.directive.js
+++ app/doc_builder.directive.js
@@ -438,6 +438,121 @@
     '<doc-builder-tab flex layout="column" ng-model="copy" ng-show="!selectedInterface" edit="edit" interfaceList="interfaces" int-names="intNames"></doc-builder-tab>'*/;
 
 
+    function deepCompare () {
+    var i, l, leftChain, rightChain;
+
+    function compare2Objects (x, y) {
+      var p;
+
+      // remember that NaN === NaN returns false
+      // and isNaN(undefined) returns true
+      if (isNaN(x) && isNaN(y) && typeof x === 'number' && typeof y === 'number') {
+           return true;
+      }
+
+      // Compare primitives and functions.
+      // Check if both arguments link to the same object.
+      // Especially useful on the step where we compare prototypes
+      if (x === y) {
+          return true;
+      }
+
+      // Works in case when functions are created in constructor.
+      // Comparing dates is a common scenario. Another built-ins?
+      // We can even handle functions passed across iframes
+      if ((typeof x === 'function' && typeof y === 'function') ||
+         (x instanceof Date && y instanceof Date) ||
+         (x instanceof RegExp && y instanceof RegExp) ||
+         (x instanceof String && y instanceof String) ||
+         (x instanceof Number && y instanceof Number)) {
+          return x.toString() === y.toString();
+      }
+
+      // At last checking prototypes as good as we can
+      if (!(x instanceof Object && y instanceof Object)) {
+          return false;
+      }
+
+      if (x.isPrototypeOf(y) || y.isPrototypeOf(x)) {
+          return false;
+      }
+
+      if (x.constructor !== y.constructor) {
+          return false;
+      }
+
+      if (x.prototype !== y.prototype) {
+          return false;
+      }
+
+      // Check for infinitive linking loops
+      if (leftChain.indexOf(x) > -1 || rightChain.indexOf(y) > -1) {
+           return false;
+      }
+
+      // Quick checking of one object being a subset of another.
+      // todo: cache the structure of arguments[0] for performance
+      for (p in y) {
+          if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+              return false;
+          }
+          else if (typeof y[p] !== typeof x[p]) {
+              return false;
+          }
+      }
+
+      for (p in x) {
+          if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+              return false;
+          }
+          else if (typeof y[p] !== typeof x[p]) {
+              return false;
+          }
+
+          switch (typeof (x[p])) {
+              case 'object':
+              case 'function':
+
+                  leftChain.push(x);
+                  rightChain.push(y);
+
+                  if (!compare2Objects (x[p], y[p])) {
+                      return false;
+                  }
+
+                  leftChain.pop();
+                  rightChain.pop();
+                  break;
+
+              default:
+                  if (x[p] !== y[p]) {
+                      return false;
+                  }
+                  break;
+          }
+      }
+
+      return true;
+    }
+
+    if (arguments.length < 1) {
+      return true; //Die silently? Don't know how to handle such case, please help...
+      // throw "Need two or more arguments to compare";
+    }
+
+    for (i = 1, l = arguments.length; i < l; i++) {
+
+        leftChain = []; //Todo: this can be cached
+        rightChain = [];
+
+        if (!compare2Objects(arguments[0], arguments[i])) {
+            return false;
+        }
+    }
+
+    return true;
+  };
+
   var isEmpty = function(element) {
     var type = getType(element);
     if(type=='array') {
@@ -944,12 +1059,20 @@
         $scope.changeEqualItems = function() {
           if($scope.selectedSchema.type == 'array') {
               if($scope.selected.equalItems && getType($scope.selectedSchema.items) == 'array') {
-                $scope.selectedSchema.items = $scope.selectedSchema.items.reduce(function(memo, item, i) {
+                var reduced = $scope.selectedSchema.items.reduce(function(memo, item, i) {
+                  if(memo.one) {
+                    if(!memo.one.type) {
+                      memo.one = item;
+                    } else if(!deepCompare(memo.one, item)) {
+                      delete memo.one;
+                    }
+                  }
                   var tmpElem = {};
                   tmpElem['property_'+i] = item;
-                  memo.properties = angular.merge(memo.properties, item.properties||tmpElem);
+                  memo.many.properties = angular.merge(memo.many.properties, item.type=='object'?item.properties:tmpElem);
                   return memo;
-                }, {type: 'object', properties: {}});
+                }, {one: {}, many: {type: 'object', properties: {}}});
+                $scope.selectedSchema.items = reduced.one||reduced.many;
                 $scope.selected.parent[$scope.selected.key].forEach(function(item, i, parent) {
                   parent[i] = buildDocument($scope.selectedSchema.items);
                 });
